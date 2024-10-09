8d648774e697e28b8b802731f6e31da696d57e50
mahmoud kamel <spop66009@gmail.com>
2024-10-09 15:14:23 +0300

    fix the protect route issue

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,6 @@
 'use client'
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import withAuth from './withAuth';
 
 function Home() {
   const [pageData, setPageData] = useState({ name: '', description: '', image: '', instructions: '' });
